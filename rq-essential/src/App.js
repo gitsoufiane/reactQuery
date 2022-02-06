9332e0d7ac19d097a7ac706454962e40d52d2567
Soufiane <62034446+gitsoufiane@users.noreply.github.com>
2022-02-06 15:16:44 -0800

    rq essential 1- 10

--- rq-essential/src/App.js
+++ rq-essential/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchPokemon = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  return axios
+    .get('https://pokeapi.co/api/v2/pokemon')
+    .then((res) => res.data.results);
+};
+
+function Pokemon({ queryKey }) {
+  const query = useQuery(queryKey, fetchPokemon, {
+    //refetch on window focus
+    refetchOnWindowFocus: true,
+
+    // query in stale get refetched automatically in the background (ready to get refetch)
+    // query will be considered fresh for  5 seconds before marked as stale
+    // fresh query will not be refetched automatically in the background
+    staleTime: 5000,
+
+    // The time in milliseconds that unused/inactive cache data remains in memory
+    cacheTime: 3000,
+  });
+  const { data, isSuccess, isError, isLoading, isFetching } = query;
+  console.log(query);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error :(</p>;
+
+  if (isSuccess)
+    return (
+      <div className="App">
+        {isSuccess &&
+          data.map((pokemon) => <div key={pokemon.name}>{pokemon.name}</div>)}
+        {isFetching && <p>Fetching...</p>}
+      </div>
+    );
+}
 
 function App() {
+  const [toggle, setToggle] = React.useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setToggle(!toggle)}>Toggle</button>
+      {toggle && (
+        <>
+          <Pokemon queryKey='pokemon' />
+        </>
+      )}
     </div>
   );
 }
