be45dc59554c4194e041d16f8102820770c27bcf
soufiane chaoufi <soufiane.chaoufi@gmail.com>
2022-03-13 16:01:33 -0700

    onMutate callback

--- rq-essential/src/components/Posts/PostForm.jsx
+++ rq-essential/src/components/Posts/PostForm.jsx
@@ -10,14 +10,26 @@ export function PostForm() {
   const mutate = useMutation(
     (post) => axios.post('http://localhost:3001/posts', post),
     {
+      onMutate: (post) => {
+        client.cancelQueries(['posts', 1]); // cancel all queries for this key ['posts', 1]
+        const oldData = client.getQueryData(['posts', 1]); // get query data for this key ['posts', 1]
+        console.log({ oldData });
+        // client.setQueryData(['posts', 1], (posts) => [
+        //   ...posts,
+        //   { ...post, id: Math.random() },
+        // ]);
+        return () => client.setQueryData(['posts', 1], oldData); //pass to context in onError , onSuccess , onSettled
+      },
       onSuccess: (data, variables, context) => {
         client.invalidateQueries('posts');
-        console.log('onSuccess',{ data, variables, context })
+        console.log('onSuccess', { data, variables, context });
       },
-      onError: (error, variables, context) =>
-        console.log({ error, variables, context }),
+      onError: (error, variables, context) => {
+        console.log('onError', { error, variables, context });
+        if (context) context();
+      },
       onSettled: (data, error, variables, context) =>
-        console.log('onSettled',{ data, error, variables, context }),
+        console.log('onSettled', { data, error, variables, context }),
     },
   );
 
